51ccde8b208641c50699f69010276bd54d34882d
Leonardo Iania <leonardo.iania01@icatt.it>
2023-03-27 17:15:10 +0200

    created streamlit app

--- app.py
+++ app.py
@@ -1,46 +1,34 @@
-from flask import Flask,request,render_template,flash
+import streamlit as st
+from PIL import Image
+import dill
 from src.pipeline.predict_pipeline import PredictPipeline
-import io
-import base64
-from werkzeug.utils import secure_filename
+from src.pipeline.preprocess_pipeline import data_transform
 
-
-import PIL
-
-
-app = Flask(__name__)
-app.secret_key = 'secret'
-
-
-
-@app.route('/upload',methods=['GET', 'POST'])
-def upload():
-    if request.method=='GET':
-        return render_template('home.html')
-    else:
-        try:
-            file = request.files['image']
-            
-                        
-            predictor = PredictPipeline()
-            preds = predictor.predict(file)
-            pred_age = int(preds)
-
-            # pic = PIL.Image.open(io.BytesIO(file.read()))
-            img = PIL.Image.open(file) 
-            data = io.BytesIO()
-            img.save(data, "JPEG")
-            encoded_img = base64.b64encode(data.getvalue())
-            decoded_img = encoded_img.decode('utf-8')
-            img_data = f"data:image/jpeg;base64,{decoded_img}"
-
-            return render_template('home.html',pred_age =pred_age,picture=img_data)
-
+def predict(testimg):
+    try:    
+        predictor = PredictPipeline()
+        preds = predictor.predict(testimg)
+        pred_age = int(preds)
+        st.success(f'Predicted age: {pred_age}')
+        return pred_age
+    except:
+        st.title(' ')
         
-        except PIL.UnidentifiedImageError:
-            flash('Error: Please upload a valid image file.')
-            return render_template('home.html',pred_age = '77777')
+
+# model = joblib.load('xgbpipe.joblib')
+st.title("Upload the photo and then click 'Predict the age' ")
+
+try:
+    trigger = st.button('Predict the age', on_click=predict(file))
+except:
+    file = st.file_uploader(" ")
+    trigger = st.button('Predict the age', on_click=predict(file))
 
 
-if __name__=="__main__":
-    app.run(debug='True',host="0.0.0.0")     
+try:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+except:
+    st.title(' ')
+
+
